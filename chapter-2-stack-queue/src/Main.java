5336a0b68182ff705eaa30a408b2eb4e65e233db
sagwa <jerrysagwa0608@gmail.com>
2024-01-03 14:30:41 +0800

    Stack & Queue

---
+++ chapter-2-stack-queue/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new ArrayStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+        Queue<Integer> queue = new LoopQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+
+    }
+}
